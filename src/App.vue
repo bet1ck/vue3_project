e73176211a62f8cc3475c31532851f0b4f7f5374
Avanesyants <albertos2012@yandex.ru>
2024-02-19 02:40:48 +0300

    2-00-26

--- src/App.vue
+++ src/App.vue
@@ -27,6 +27,24 @@
             v-if="!isPostsLoading"
         />
         <div v-else>Идет загрузка...</div>
+        <div
+            class="observer"
+            ref="observer"
+        >
+        </div>
+        <!-- <div class="page__wrapper">
+            <div
+                v-for="pageNumber in totalPages"
+                :key="pageNumber"
+                class="page"
+                :class="{
+                    'current-page': page === pageNumber
+                }"
+                @click="changePage(pageNumber)"
+            >
+                {{ pageNumber }}
+            </div>
+        </div> -->
     </div>
 </template>
 
@@ -51,6 +69,9 @@ export default{
             isPostsLoading: false,
             selectedSort: '',
             searchQuery: '',
+            page: 1,
+            limit: 10,
+            totalPages: 0,
             sortOptions: [
                 {value: 'title', name: 'По названию'},
                 {value: 'body', name: 'По содержанию'},
@@ -68,11 +89,21 @@ export default{
         showDialog() {
             this.dialogVisible = true
         },
+        // changePage(pageNumber){
+        //     this.page = pageNumber
+        //     this.fetchPost()
+        // },
         async fetchPost(){
             try {
                 this.isPostsLoading = true;
                 setTimeout( async()=>{
-                    const response = await axios.get( 'https://jsonplaceholder.typicode.com/posts?_limit=10');
+                    const response = await axios.get( 'https://jsonplaceholder.typicode.com/posts',{
+                        params: {
+                            _page: this.page,
+                            _limit: this.limit,
+                        }
+                    });
+                    this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
                     this.posts = response.data
                 },1000)
             } catch (e) {
@@ -81,9 +112,38 @@ export default{
                 this.isPostsLoading = false;
             }
         },
+        async loadMorePosts(){
+            try {
+                this.page += 1;
+                 async()=>{
+                    const response = await axios.get( 'https://jsonplaceholder.typicode.com/posts',{
+                        params: {
+                            _page: this.page,
+                            _limit: this.limit,
+                        }
+                    });
+                    this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
+                    this.posts = [...this.posts, ...response.data];
+                }
+            } catch (e) {
+                alert('Ошибка')
+            }
+        },
     },
     mounted() {
         this.fetchPost();
+        const options = {
+            rootMargin: "0px",
+            threshold: 1.0,
+        };
+        const callback = (entries, observer) => {
+            if (entries[0].isIntersecting && this.page < this.totalPages) {
+                this.loadMorePosts()
+                console.log("gthtctr");
+            }
+        };
+        const observer = new IntersectionObserver(callback, options);
+        observer.observe(this.$refs.observer);
     },
     computed: {
         sortedPosts(){
@@ -95,6 +155,11 @@ export default{
             return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
         },
     },
+    watch: {
+        // page(){
+        //     this.fetchPost()
+        // }
+    }
 }
 </script>
 
@@ -116,5 +181,28 @@ export default{
     display: flex;
     justify-content: space-between;
     margin: 15px 0;
+}
+
+.page__wrapper
+{
+    display: flex;
+    margin-top: 15px;
+}
+
+.page
+{
+    border: 1px solid black;
+    padding: 5px;
+}
+
+.current-page
+{
+    border: 2px solid green;
+}
+
+.observer
+{
+    height: 30px;
+    background: green;
 }
 </style>
---
+++ src/pages/Main.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>
+        добро пожаловать на курс vue3
+    </h1>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+    
+</style>
